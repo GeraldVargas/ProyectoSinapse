6260a727c542763ed071f59d839e1423ee7cc9d1
Geradl Vargas <geraldvargasmorales@gmail.com>
2025-10-12 23:45:35 -0500

    Primer commit del proyecto

---
+++ resources/js/components/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '20px', color: 'black', backgroundColor: 'white' }}>
+      <h1>Hola Gerald, mi página funciona!</h1>
+      <p>Esto se muestra desde React dentro de Laravel.</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ resources/js/pages/gordo.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MiPagina = () => {
+  return (
+    <div style={{ padding: '20px', color: 'black', backgroundColor: 'white' }}>
+      <h1>Papus</h1>
+      <p>salvador cara de poto</p>
+    </div>
+  );
+};
+
+export default MiPagina;
