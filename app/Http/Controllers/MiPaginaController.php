3294b3bbfe329b437b165c7f1f85efa5e6d14809
Geradl Vargas <geraldvargasmorales@gmail.com>
2025-10-16 02:41:49 -0500

    Corrigiendo dependencias

--- app/Http/Controllers/MiPaginaController.php
+++ app/Http/Controllers/MiPaginaController.php
@@ -7,5 +7,9 @@ use Illuminate\Http\Request;
 
 class MiPaginaController extends Controller
 {
-    //
+    public function index()
+    {
+        // Esto carga la plantilla Blade que a su vez renderiza React
+        return view('app');
+    }
 }
